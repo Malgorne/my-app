ed0a596897c6120292065f3ecc0f10626554476b
Benjamin FRITZ <benjamin.fritz.prestataire@sfr.com>
2018-06-06 12:27:15 +0200

    Install sass

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
+import './style.sass';
 
 export default class Home extends React.Component {
   render() {
--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,8 @@ module.exports = {
         loader: 'babel-loader'
       }
     }, {
-      test: /\.css$/,
-      use: ['style-loader','css-loader']
+      test: /\.sass$/,
+      use: ['style-loader','css-loader', 'sass-loader']
     }]
   },
   plugins: [htmlPlugin]
