06a647c1602278ac8250d6c2088b6a763e718ca1
Malgorne <fritz.benj@free.fr>
2018-06-08 16:57:32 +0200

    Removes logs, cleans src/index

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import './style.sass';
 
 import { Home } from 'screens';
-console.log('Home', Home);
-const Index = () => <Home />;
 
 ReactDOM.render(
   <div id="main">
     <p>MAIN</p>
-    <Index />
+    <Home />
   </div>,
   document.getElementById('index')
 );
