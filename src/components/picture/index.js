a8ccfeb42e679e355f9bcd81538698541655ebc0
Malgorne <fritz.benj@free.fr>
2018-06-08 16:45:43 +0200

    Add tests

--- src/components/picture/index.js
+++ src/components/picture/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import img from '../../assets/img/mario.jpeg';
+import img from '../../assets/img/under-construction.png';
 
 import './style.sass';
 
@@ -13,7 +13,7 @@ export default class Picture extends React.Component {
   render() {
     return (
       <div className="picture">
-        <img src={img} alt="Smiley face" />
+        <img src={img} alt="under construction" />
       </div>
     );
   }
--- src/screens/home/home.spec.js
+++ src/screens/home/home.spec.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { Home } from 'screens';
+import renderer from 'react-test-renderer';
 
 /**
  * Unit tests for component/home.
@@ -6,45 +8,8 @@ import { Home } from 'screens';
 describe('components/home', () => {
   it('should be defined', () => expect(Home).toBeDefined());
   it('should initialize the component', () => expect(new Home()).toBeDefined());
-
-  /**
-   * Test a single element.
-   * @param {Object} objectTested React element to test.
-   * @param {Object} tests expected result.
-   */
-  const testElement = (objectTested, tests) => {
-    expect(objectTested.type).toBe(tests.type);
-    for ( const property in tests) {
-      if (property !== 'children' && property !== 'type') {
-        expect(objectTested.props[property]).toBe(tests[property]);
-      }
-    }
-  };
-
-  /**
-   * Test render's semantic.
-   * @param {Object} objectTested React element rendered to test.
-   * @param {Object} expected expected result.
-   */
-  const testSemantic = (objectTested, expected) => {
-    testElement(objectTested, expected);
-    if (expected.children) {
-      expect(objectTested.props.children.length).toBe(expected.children.length);
-      for (let i = 0; expected.children[i]; i++) {
-        testSemantic(objectTested.props.children[i], expected.children[i]);
-      }
-    }
-  };
   it('should respect the semantic', () => {
-    const expectedJsx = {
-      type: 'div',
-      className: 'home',
-      children: [{
-        type: 'h1'
-      }, {
-        type: 'p'
-      }]
-    };
-    testSemantic(new Home().render(), expectedJsx);
+    const home = renderer.create(<Home />);
+    expect(home).toMatchSnapshot();
   });
 });
