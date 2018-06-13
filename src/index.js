af701bac5d7f6b644747d3822a10de3e50d866e6
Malgorne <fritz.benj@free.fr>
2018-06-13 15:41:56 +0200

    Start routing

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './style.sass';
 
-import { Home, Header, Footer } from 'screens';
+import { TicTacToe, Home, Header, Footer } from 'screens';
 
 ReactDOM.render(
   <div id="main">
     <Header />
     <BrowserRouter>
-      <Home />
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/tictactoe" component={TicTacToe}/>
+      </Switch>
     </BrowserRouter>
     <Footer />
   </div>,
   document.getElementById('index')
 );
+// <Route exact path='/' component={Home}/>
+//   {/* both /roster and /roster/:number begin with /roster */}
+//   <Route path='/roster' component={Roster}/>
+//   <Route path='/schedule' component={Schedule}/>
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './style.sass';
 
@@ -19,6 +20,7 @@ export default class Home extends React.Component {
     return (
       <div id="home">
         <p>HOME WORKS</p>
+        <Link to={'/tictactoe'}>TIC-TAC-TOE</Link>
       </div>
     );
   }
