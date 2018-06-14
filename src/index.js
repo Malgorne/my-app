b14d9f2dcad2c171140a1f6ee33690409cda283f
Malgorne <fritz.benj@free.fr>
2018-06-14 13:48:10 +0200

    Add react-bootstrap

--- src/index.js
+++ src/index.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Grid, Row, Col } from 'react-bootstrap';
 import createBrowserHistory from 'history/createBrowserHistory';
 
 import './style.sass';
 
 import { TicTacToe, Home, Header, Footer } from 'screens';
 
-const ownHistory = createBrowserHistory();
+const appHistory = createBrowserHistory();
 
 ReactDOM.render(
-  <div id="main" className="col-xs-offset-1 col-xs-10">
-    <Header/>
-    <BrowserRouter histrory={ownHistory}>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/tictactoe" component={TicTacToe}/>
-        <Route render={() => <div>Not Found</div>}/>
-      </Switch>
-    </BrowserRouter>
-    <Footer/>
-  </div>,
+  <Grid id="main" fluid>
+    <Row componentClass="section">
+      <Col sm={12}>
+        <Header/>
+      </Col>
+    </Row>
+    <Row componentClass="section">
+      <Col sm={12}>
+        <BrowserRouter histrory={appHistory}>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/tictactoe" component={TicTacToe}/>
+            <Route render={() => <div>Not Found</div>}/>
+          </Switch>
+        </BrowserRouter>
+      </Col>
+    </Row>
+    <Row componentClass="section">
+      <Col sm={12}>
+        <Footer/>
+      </Col>
+    </Row>
+  </Grid>,
   document.getElementById('index')
 );
