5842de0e34dfa73cb7ca939f6dad216dd54fd7d8
Malgorne <fritz.benj@free.fr>
2018-06-14 11:59:25 +0200

    Add history

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 
 import './style.sass';
 
 import { TicTacToe, Home, Header, Footer } from 'screens';
 
+const ownHistory = createBrowserHistory();
+
 ReactDOM.render(
   <div id="main">
     <Header/>
-    <BrowserRouter>
+    <BrowserRouter histrory={ownHistory}>
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/tictactoe" component={TicTacToe}/>
