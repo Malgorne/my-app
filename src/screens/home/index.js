78502b76d5981580f6597dfc94ad6435629578f1
Malgorne <fritz.benj@free.fr>
2018-06-14 13:10:50 +0200

    Removes class

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -18,7 +18,7 @@ export default class Home extends React.Component {
    */
   render() {
     return (
-      <div id="home" className="btn btn-warning">
+      <div id="home">
         <h1>HOME WORKS</h1>
         <Link to={'/tictactoe'} className="btn btn-warning">TIC-TAC-TOE</Link>
       </div>
