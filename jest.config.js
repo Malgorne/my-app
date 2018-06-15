437bb7506984a0e858e867e77655405d32848968
Malgorne <fritz.benj@free.fr>
2018-06-15 16:41:32 +0200

    Add unit tests

--- jest.config.js
+++ jest.config.js
@@ -13,5 +13,13 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '^components(.*)': '<rootDir>/src/components$1',
     '^screens(.*)': '<rootDir>/src/screens$1'
+  },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
   }
 };
--- src/components/head-carousel/headCarousel.spec.js
+++ src/components/head-carousel/headCarousel.spec.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { HeadCarousel } from 'components';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 /**
  * Unit tests for components/head-carousel.
  */
 describe('components/head-carousel', () => {
   it('should render without crashing', () => {
+    // without params
     shallow(<HeadCarousel />);
+    // With right params
+    const items = [{
+      src: 'path/to/img1',
+      alt: 'img1',
+      title: 'title img1',
+      subTitle: 'subTitle img1'
+    }, {
+      src: 'path/to/img2',
+      alt: 'img2',
+      title: 'title img2',
+      subTitle: 'subTitle img2'
+    }, {
+      src: 'path/to/img3',
+      alt: 'img3',
+      title: 'title img3',
+      subTitle: 'subTitle img3'
+    }];
+    shallow(<HeadCarousel items={items}/>);
   });
 });
